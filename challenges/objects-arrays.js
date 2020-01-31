5220cb2c296036b446662497030cee156d5a8c8c
M Groesbeck <m.groesbeck@gmail.com>
2020-01-31 08:54:06 -0800

    Completed objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -30,6 +30,7 @@ const velociraptor = {
   length: '1.8m',
   period: 'Late Cretaceous'
 }
+
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
@@ -46,9 +47,11 @@ console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
+
 tyrannosaurus.roar = function(){
   return 'RAWERSRARARWERSARARARRRR!';
 }
+
 console.log(tyrannosaurus.roar());
 
 
@@ -72,7 +75,9 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
+
 const universities = graduates.map((item)=>item.university);
+
 console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -81,13 +86,15 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
+
 const contactInfo = graduates.map((item)=>`${item.first_name} ${item.email}`);
+
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = graduates.filter(function(item){
-  return item.university.includes('Uni');
-})
+
+const unisWithUni = graduates.filter((item) => item.university.includes('Uni'));
+
 console.log(unisWithUni);
 
 
